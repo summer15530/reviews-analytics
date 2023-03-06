77314a6413b9f8d890891b745b1af140f914fda7
Mars <summer15530@gmail.com>
2023-03-07 05:31:50 +0800

    word count function

--- read.py
+++ read.py
@@ -1,25 +1,71 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
 
 print(f"檔案讀取完了，總共有{len(data)}筆資料")
 
+
+print(data[0])
+
 sum_len = 0
 for d in data:
-	sum_len = sum_len + len(d)
+    sum_len = sum_len + len(d)
 
 print(f'留言的平均長度為，{sum_len/len(data)}')
 
 
 new = []
 for d in data:
-	if len (d) < 100:
-		new.append(d)
-print(f'一共有 {len(new)} 筆留言長度小於100')
+    if len (d) < 100:
+    	new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '')
+
+
+	#文字記數
+wc = {} # wc = word count 這行先創一個字典
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])
+
+
+print(len(wc))
+print(wc['Allen'])
+
+
+while True:
+    word = input('請問你想查甚麼字 : ')
+    if word == 'q':
+        break
+        if word in wc:
+            print(word, '出現過的次數為 : ', wc[word])
+    else:
+    	print('這個字沒有出現過喔')
+
+print('感謝使用')
+
+
+
+
+
